1a0dcd0537bb3f06b0def05e56f17d8ad56eac14
Lisa.Zhang <346774987@qq.com>
2026-03-05 11:18:33 +0800

    update gunicorn.conf.py

--- release/conf/gunicorn.conf.py
+++ release/conf/gunicorn.conf.py
@@ -16,7 +16,6 @@ timeout = 120
 worker_class = 'sync'
 
 workers = multiprocessing.cpu_count()  # 进程数
-threads = 2
 loglevel = 'info'  # 日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
 access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'  # 设置gunicorn访问日志格式，错误日志无法设置
 

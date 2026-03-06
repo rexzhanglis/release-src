2f50484fb0ca83606595ef7ed7733cd043079b8d
Lisa.Zhang <346774987@qq.com>
2026-03-06 11:11:33 +0800

    fixed mdl-admin

--- release/mdl/admin.py
+++ release/mdl/admin.py
@@ -1,40 +1,28 @@
-from django import forms
 from django.contrib import admin
 
-from mdl.models import MdlServer, Label
+from mdl.models import MdlServer, Host, Label
 
 
-class MdlServerForm(forms.ModelForm):
-
-    def clean(self):
-        from external.cmdb_client import CmdbClient
-        cleaned_data = super().clean()
-        data = cleaned_data['config_git_url']
-        # 1. 生产环境机器请填写git对应的配置文件路径信息
-        if not data and CmdbClient().get_server_info_by_ip(ip=cleaned_data['ip'])["env"] == 'prod':
-            raise forms.ValidationError('生产环境机器请填写git对应的配置文件路径信息')
-        if data:
-            # 2. 非生产环境机器请不要填写git配置文件
-            if CmdbClient().get_server_info_by_ip(ip=cleaned_data['ip'])["env"] != 'prod':
-                raise forms.ValidationError('非生产环境机器请不要填写git配置文件')
-            # 3. consul space与git配置文件不一致
-            mdl_consul_base_path = "http://consul.wmcloud.com/v1/kv/configs/mdl/"
-            config_git_path = str(cleaned_data['consul_space']).lstrip(mdl_consul_base_path) + cleaned_data['consul_files']
-            if config_git_path not in data:
-                raise forms.ValidationError('consul space与git配置文件不一致')
-        return cleaned_data
-
-    class Meta:
-        model = MdlServer
-        fields = '__all__'
+@admin.register(Host)
+class HostAdmin(admin.ModelAdmin):
+    list_display = ['fqdn', 'ip', 'user', 'remote_python', 'init_status', 'created_time']
+    search_fields = ('fqdn', 'ip')
+    ordering = ('fqdn',)
 
 
 @admin.register(MdlServer)
 class MdlServerAdmin(admin.ModelAdmin):
-    list_display = ['fqdn', 'role_name', 'ip', 'user', 'remote_python', 'config_git_url', 'consul_space',
-                    'consul_token', 'install_dir', 'backups_dir', 'service_name', 'is_consistent']
-    search_fields = ('fqdn', 'role_name', 'ip')
-    form = MdlServerForm
+    list_display = ['get_fqdn', 'role_name', 'get_ip', 'service_name', 'install_dir',
+                    'consul_space', 'consul_token', 'backups_dir', 'is_consistent', 'init_status']
+    search_fields = ('host__fqdn', 'host__ip', 'role_name', 'service_name')
+
+    @admin.display(description='FQDN', ordering='host__fqdn')
+    def get_fqdn(self, obj):
+        return obj.host.fqdn
+
+    @admin.display(description='IP 地址', ordering='host__ip')
+    def get_ip(self, obj):
+        return obj.host.ip
 
 
 @admin.register(Label)

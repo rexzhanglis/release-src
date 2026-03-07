1a48e26167dd4f75ea05a615cd1e8140d1d2b382
yinyin.zhang <yinyin.zhang@datayes.com>
2026-03-07 10:03:18 +0800

    add dist

---
+++ vue-release-web/dist/config.js
@@ -0,0 +1,9 @@
+window.config = {
+  BACKEND_BASE_API: "http://localhost:8000/api",
+  WEB_LOGIN_URL: "http://localhost:9528/",
+  CAS_LOGOUT_URL: "https://cas.wmcloud.com/cas/logout",
+  BACKEND_LOGIN_URL: "http://localhost:8000/login",
+  DASHBOARD_GRAFANA_MONITOR_URL: "http://grafana.devops2.wmcloud.com/d/OLK0tU8nk/fa-bu-dashboard?orgId=1&theme=light&kiosk=tv",
+  MDL_LABEL_URL:"http://localhost:8000/adminmdl/label/",
+}
+
